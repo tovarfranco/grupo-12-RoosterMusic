1dd53cb4789037c00cf9ca429ec5bbaf7d5495ba
Franco Tovar <franco.tovar.ext@kavak.com>
2021-08-14 17:48:24 -0300

    feat: folder structure, html and node server

---
+++ Project/app.js
@@ -0,0 +1,15 @@
+const express = require('express');     // Levantar/crear un servidor.
+const app = express();                  // Debemos guardar la ejecución de la función en un variable llamada app generalmente.
+
+const path = require('path');           // Me permite independizar del sistema operativo y que no haya inconvenientes con las rutas.
+
+app.use(express.static(path.resolve(__dirname, './public')));   // Indicamos a Express que esta carpeta será pública.
+
+const puerto = process.env.PORT || 8080
+
+app.listen(puerto, () => console.log('Servidor corriendo en el puerto 8080'));   // Levantamos el servidor que estará escuchando peticiones en el puerto 3000.
+
+// Ruta para la raíz:
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/index.html'))
+})
