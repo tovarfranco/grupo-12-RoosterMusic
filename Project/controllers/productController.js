efa91db05575325ce02da59675247d22b98d016c
Alan Victor Tovar <alanvictortovar@hotmail.com>
2021-09-08 02:05:00 -0300

    feat: MVC created and working + ejs

--- Project/controllers/productController.js
+++ Project/controllers/productController.js
@@ -4,7 +4,7 @@ const productController = {
 
     productDetail: (req,res) => {                                                 // ACA se pone el callback que sacamos de ROUTES. Este será el encargado de generar la respuesta.
         const product = productList.find(product => product.id == req.params.id)  // Busca en la lista el plato que tiene el mismo id que el pasado por parámetro.
-        res.render('productDetail', {product: product})                           // Le envía a ESTA view las variables dinámicas que necesita.  // ESTE ES UN .EJS (modificar los .html a .ejs). Es NECESARIO setear el VIEW ENGINE en app.js para usar res.render(). // Le envía a ESTA view las variables dinámicas que necesita.
+        res.render('productDetail', {product: product, productList: productList})                           // Le envía a ESTA view las variables dinámicas que necesita.  // ESTE ES UN .EJS (modificar los .html a .ejs). Es NECESARIO setear el VIEW ENGINE en app.js para usar res.render(). // Le envía a ESTA view las variables dinámicas que necesita.
     }
 }
 
