b27698d5edb42c73881a317d8ec707b0acd0e9c3
Alan Victor Tovar <alanvictortovar@hotmail.com>
2021-10-04 02:26:33 -0300

    feat: express-validation, general validations completed, before login process

--- Project/controllers/user.controller.js
+++ Project/controllers/user.controller.js
@@ -2,23 +2,23 @@
 const fs = require('fs');                                                        // Para la lectura y escritura de archivos.
 const path = require('path');                                                    // Manejo de rutas.
 const {validationResult} = require('express-validator');                         // Tambien se requiere acá. Solo nos interesa el elemento validationResult de espress-validator (destructuring).
+const bcryptjs = require('bcryptjs');                                            // Para encriptar las contraseñas.
 
-// =========== Lectura BBDD ===========================
-const userListPath = path.join(__dirname, "../database/userList.json");    // Ruta del archivo BBDD.
-let userList = JSON.parse(fs.readFileSync(userListPath, 'utf-8'));         // Importamos la BBDD que se pasará a las vistas. Lista de objetos.
+// =========== Modelo ============================
+const User = require('../models/User.model.js');
 
 // =========== Controlador ============================
 const userController = {
 
     /*** Logueo ***/
-    login: (req,res) => {
+    login: (req, res) => {
         res.render('login');
     },
 
     /*** Detalle de un usuario ***/
-    detail: (req,res) => {
-        const user = userList.find(user => user.id == req.params.id);
-        res.render('userDetail', {user: user});
+    detail: (req, res) => {
+        let userFound = User.findByPk(req.params.id)                             // Llamo al modelo.
+        res.render('userDetail', {user: userFound});
     },
 
     /*** Creo un usuario ***/
@@ -27,81 +27,78 @@ const userController = {
 	},
 
 	store: (req, res) => {
-
-        const resultValidation = validationResult(req);                 // Guardamos los resultados de las validaciones. Es un ARRAY de objetos que tiene los errores que se produjeron (input name, mensaje, etc).
-
-		if (!resultValidation.isEmpty()) {                              // Si hubo errores, devuelvo la vista con los errores + la data ya ingresada del formulario.
+        /* Verifico errores por express-validator */
+        const resultValidation = validationResult(req);                          // Guardamos los resultados de las validaciones. Es un ARRAY de objetos que tiene los errores que se produjeron (input name, mensaje, etc).
+        
+		if (!resultValidation.isEmpty()) {                                       // Si hubo errores, devuelvo la vista con los errores + la data ya ingresada del formulario.
 			return res.render('register', {errors: resultValidation.mapped(), oldData: req.body});   // .mapped() convierte a ese array en un OBJETO literal con claves el input name y valor el contenido de todo ese error. Paso la data del body nuevamente así lo la pierdo.
 		}
 
-		let imagenName;                                                 // Para guardar la imagen. Si existe uso su nombre sino uso el default.
+        /* Verifico si ya existe el usuario (mismo email) */
+        let userFound = User.findByField('email', req.body.email);               // Uso esta función que creé en el modelo para verificar ya existe.
+
+		if (userFound) {
+			return res.render('register', {errors: {email: {msg: 'Este email ya está registrado'}}, oldData: req.body});    // Creo una valicación propia. Ver que creo el objeto errors por mi cuenta con su mensaje.
+		}
+
+        /* Sin errores, continúo con la lógica */
+        /* Imagen */
+		let imagenName;                                                          // Para guardar la imagen. Si existe uso su nombre sino uso el default.
 		if (req.file) {
 			imagenName = req.file.filename;
 		} else {
 			imagenName = "img-sin-imagen-disponible.jpg"
 		}
 
-		let nuevo = {
-			id: userList.slice(-1)[0].id + 1,                            // Lo hice de esta forma solo para agregar un id "autonumérico".
-            first_name: req.body.nombre,
-            last_name: req.body.apellido,
-            dni: req.body.dni,
-			country: req.body.pais,								         // Guardo el nombre de la imagen que llega de Multer. Tiene una lógica arriba.
-            address: req.body.domicilio,
-            birth: req.body.nacimiento,
-            email: req.body.email,
-            password: req.body.password,
-            image: imagenName
+        /* Inserto nuevo usuario */
+		let userToCreate = {                                                     // Creo un objeto temporal que le pasaré al modelo para que lo inserte.
+            ...req.body,
+            password: bcryptjs.hashSync(req.body.password, 10),                  // Encriptamos la contraseña. Se pisa el valor del .body por esta nueva.
+            img: imagenName
 		}
 
-		userList.push(nuevo);
-		fs.writeFileSync(userListPath, JSON.stringify(userList, null, 4));  // De esta forma lo guarda "formateado".
+        let userCreated = User.create(userToCreate);                             // Llamo al modelo.
 
-		res.redirect('/users/detail/' + nuevo.id);
+		res.redirect('/users/detail/' + userCreated.id);
 	},
 
     /*** Modifico un usuario ***/
 	edit: (req, res) => {
-		const user = userList.find(user => user.id == req.params.id);
-		res.render('userEdit', {user: user});
+		let userFound = User.findByPk(req.params.id)
+		res.render('userEdit', {user: userFound});
 	},
 
     update: (req, res) => {
-		let imagenName;                                                      // Para guardar la imagen. Si existe uso su nombre sino uso la que ya tenía.
+        /* Busco el usuario */
+        let userFound = User.findByPk(req.params.id)                             // Buscamos el usuario, me servirá para varias funciones.
+
+        /* Imagen */
+		let imagenName;                                                          // Para guardar la imagen. Si existe uso su nombre sino uso la que ya tenía.
 		if (req.file) {
 			imagenName = req.file.filename;
 		} else {
-            const user = userList.find(user => user.id == req.params.id);
-			imagenName = user.image;
+			imagenName = userFound.img;
 		}
 
-		userList.forEach(user => {
-			if (user.id == req.params.id) {
-                user.first_name = req.body.nombre,
-                user.last_name = req.body.apellido,
-                user.dni = req.body.dni,
-                user.country = req.body.pais,								// Guardo el nombre de la imagen que llega de Multer. Tiene una lógica arriba.
-                user.address = req.body.domicilio,
-                user.birth = req.body.nacimiento,
-                user.email = req.body.email,
-                user.password = req.body.password,
-                user.image = imagenName
-			}
-		});
-		
-        fs.writeFileSync(userListPath, JSON.stringify(userList, null, 4));  // De esta forma lo guarda "formateado".
+        /* Actualizo usuario */
+		let userToUpdate = {                                                     // Creo un objeto temporal que le pasaré el moddelo para que lo actualice.
+            id: userFound.id,
+            ...req.body,
+            img: imagenName
+		}
+
+        User.update(userToUpdate);                                               // Llamo al modelo.
 
 		res.redirect('/users/detail/' + req.params.id);
 	},
 
     /*** Elimino un usuario ***/
     delete: (req, res) => {
-		userList = userList.filter(user => user.id != req.params.id);
-		fs.writeFileSync(userListPath, JSON.stringify(userList, null, 4));  // De esta forma lo guarda "formateado".
+		User.delete(req.params.id);
 
 		res.redirect('/');
 	}
 }
 
 // =========== Exporto Controlador ===========================
-module.exports = userController                                      // Siempre exportarlo porque lo necesitaremos usar en el ROUTES para que sepa a qué controlador enviar la petición.
+module.exports = userController                                                  // Siempre exportarlo porque lo necesitaremos usar en el ROUTES para que sepa a qué controlador enviar la petición.
