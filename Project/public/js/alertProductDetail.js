be654126aeb3a26482597456bd7988e785bbbe2f
Alan Victor Tovar <alanvictortovar@hotmail.com>
2021-12-08 04:03:09 -0300

    JS almost finished

---
+++ Project/public/js/alertProductDetail.js
@@ -0,0 +1,39 @@
+// Utilizamos la librería sweetAlert2 para los mensajes del navegador. Simplemente es una promesa a la cual se le pasa un objeto definiendo los estilos y mensajes.
+
+$('#form-eliminar').submit(function (e) {
+    e.preventDefault();
+
+    Swal.fire({                                         // Así funciona sweetAler, dentro el objeto define los estlos.
+        title: 'Desea eliminar?',
+        text: "Esta acción no se puede revertir!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Aceptar',
+        cancelButtonText: 'Cancelar'
+    }).then((result) => {                               // Al resolverse la promesa, ejecutamos las acciones que deseamos.
+        if (result.isConfirmed) {
+            this.submit();                              // Con this apuntamos a este mismo formulario.
+        }
+    })
+});
+
+$('#form-comprar').submit(function (e) {
+    e.preventDefault();
+
+    Swal.fire({
+        title: 'Desea comprar?',
+        text: "Solo podrán usuarios logueados",
+        icon: 'question',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Aceptar',
+        cancelButtonText: 'Cancelar'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            this.submit();
+        }
+    })
+});
